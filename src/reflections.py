db60c5f25c74ba3e67ae8e19500cd2ec56b418ec
Ari Grayzel <arigrayzel@gmail.com>
2022-05-29 13:44:44 -0400

    Add cli interface to test plane calculations with

---
+++ src/reflections.py
@@ -0,0 +1,46 @@
+#!/usr/bin/env python
+
+import rospy
+import numpy as np
+import time
+import utils
+import tf
+
+from geometry_msgs.msg import Point, Quaternion
+
+
+class ReflectionTargeter(object):
+    """Listens for a target and source vector and publishes a unit vector normal to the plane that reflects source onto target"""
+    def __init__(self):
+        self.source_topic = rospy.get_param("~light_source", "/light_source")
+        self.target_topic = rospy.get_param("~target_source", "/target_source")
+        self.pub_topic = rospy.get_param("~plane_normal", "/plane_normal")
+        
+        self.source_sub = rospy.Subscriber(self.source_topic, Point, self.source_callback, queue_size=1) 
+        self.target_sub = rospy.Subscriber(self.target_topic, Point, self.target_callback, queue_size=1) 
+        self.pub = rospy.Publisher(self.pub_topic, Point, queue_size=1)
+
+        self.source = np.array([0.0,0.0,0.0])
+        self.target = np.array([0.0,0.0,0.0])
+
+    def source_callback(self, msg):
+        new_source = np.array([msg.x, msg.y, msg.z])
+        self.source = new_source/np.linalg.norm(new_source) #ensure normalization
+        self.calculater_normal()
+
+    def target_callback(self, msg):
+        new_target = np.array([msg.x, msg.y, msg.z])
+        self.target = new_target/np.linalg.norm(new_target) #ensure normalization
+        self.calculater_normal()
+
+    def calculate_normal(self):
+        diff = self.target-self.source
+        norm = diff/np.linalg.norm(diff)
+        msg = Point(norm[0], norm[1], norm[2])
+        self.pub.publish(msg)
+
+
+if __name__=="__main__":
+    rospy.init_node("normal_calc")
+    rt = ReflectionTarget()
+    rospy.spin()
